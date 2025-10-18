8cacc23011b010fedfd3de346029b77143b05619
kesshei <dagang520@qq.com>
2025-10-18 14:24:51 +0800

    提交

---
+++ 02_第二章 Direct3D程序设计基础/01_CreateDevice/CreateDevice.cpp
@@ -0,0 +1,179 @@
+//=============================================================================
+// Desc: 最简单的Direct3D程序, 
+//=============================================================================
+
+#include <d3d9.h>
+
+
+//-----------------------------------------------------------------------------
+// 全局变量
+//-----------------------------------------------------------------------------
+LPDIRECT3D9             g_pD3D = NULL; //Direct3D对象
+LPDIRECT3DDEVICE9       g_pd3dDevice = NULL; //Direct3D设备对象
+
+
+//-----------------------------------------------------------------------------
+// Desc: 初始化Direct3D
+//-----------------------------------------------------------------------------
+HRESULT InitD3D(HWND hWnd)
+{
+    D3DDEVTYPE DeviceType = D3DDEVTYPE_HAL;
+    //创建Direct3D对象, 该对象用来创建Direct3D设备对象
+    if (NULL == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
+    {
+        return E_FAIL;
+    }
+
+// 查询显示适配器的能力
+D3DCAPS9 caps;
+if (FAILED(g_pD3D->GetDeviceCaps(D3DADAPTER_DEFAULT,//默认显示适配器
+    DeviceType,// 硬件抽象层设备
+	&caps) // 输出参数, 返回显示适配器的能力
+))
+{
+    return E_FAIL;
+}
+
+DWORD BehaviorFlags = 0;
+//D3DDEVCAPS_HWTRANSFORMANDLIGHT /* 设备支持在硬件中进行变换和光照计算，并且必须同时支持 DRAWPRIMITIVES2EX */
+if (caps.DevCaps & D3DDEVCAPS_HWTRANSFORMANDLIGHT) {
+    BehaviorFlags = D3DCREATE_HARDWARE_VERTEXPROCESSING;
+}
+else {
+    BehaviorFlags = D3DCREATE_SOFTWARE_VERTEXPROCESSING;
+}
+    //获取设备类型，默认是D3DDEVTYPE_HAL类型
+    if (caps.DeviceType != D3DDEVTYPE_HAL) {
+        DeviceType = caps.DeviceType;
+    }
+    //获取当前显卡的显示模式
+    D3DDISPLAYMODE d3ddm;
+	;
+    if (FAILED(g_pD3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &d3ddm)))
+    {
+        return E_FAIL;
+    }
+
+//设置D3DPRESENT_PARAMETERS结构, 准备创建Direct3D设备对象
+D3DPRESENT_PARAMETERS d3dpp;
+ZeroMemory(&d3dpp, sizeof(d3dpp));
+d3dpp.Windowed = TRUE;
+d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+
+
+//创建Direct3D设备对象
+if (FAILED(g_pD3D->CreateDevice(
+	D3DADAPTER_DEFAULT, //默认显示适配器 
+	DeviceType, //设备类型
+    hWnd, 
+	BehaviorFlags | D3DCREATE_MULTITHREADED, //多线程支持
+	&d3dpp, //创建参数
+    &g_pd3dDevice)))
+{
+    return E_FAIL;
+}
+
+    return S_OK;
+}
+
+
+//-----------------------------------------------------------------------------
+// Desc: 释放创建对象
+//-----------------------------------------------------------------------------
+VOID Cleanup()
+{
+    //释放Direct3D设备对象
+    if (g_pd3dDevice != NULL)
+        g_pd3dDevice->Release();
+
+    //释放Direct3D对象
+    if (g_pD3D != NULL)
+        g_pD3D->Release();
+}
+
+
+//-----------------------------------------------------------------------------
+// Desc: 渲染图形
+//-----------------------------------------------------------------------------
+VOID Render()
+{
+    //清空后台缓冲区
+    g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(45, 50, 170), 1.0f, 0);
+
+    //开始在后台缓冲区绘制图形
+    if (SUCCEEDED(g_pd3dDevice->BeginScene()))
+    {
+        //在此在后台缓冲区绘制图形
+
+        //结束在后台缓冲区渲染图形
+        g_pd3dDevice->EndScene();
+    }
+
+    //将在后台缓冲区绘制的图形提交到前台缓冲区显示
+    g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+
+//-----------------------------------------------------------------------------
+// Desc: 消息处理
+//-----------------------------------------------------------------------------
+LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        Cleanup();
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        Render();
+        ValidateRect(hWnd, NULL);
+        return 0;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+
+//-----------------------------------------------------------------------------
+// Desc: 程序入口
+//-----------------------------------------------------------------------------
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
+{
+    //注册窗口类
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L,GetModuleHandle(NULL), NULL, NULL, NULL, NULL, L"ClassName", NULL };
+    RegisterClassEx(&wc);
+
+    //创建窗口
+    HWND hWnd = CreateWindow(L"ClassName", L"最简单的Direct3D程序",WS_OVERLAPPEDWINDOW, 200, 100, 600, 500,NULL, NULL, wc.hInstance, NULL);
+
+    //初始化Direct3D
+    if (SUCCEEDED(InitD3D(hWnd)))
+    {
+        //显示主窗口
+        ShowWindow(hWnd, SW_SHOWDEFAULT);
+        UpdateWindow(hWnd);
+
+        //进入消息循环
+        MSG msg;
+        ZeroMemory(&msg, sizeof(msg));
+        while (msg.message != WM_QUIT)
+        {
+            if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+            else
+            {
+                Render();  //渲染图形
+            }
+        }
+    }
+
+    UnregisterClass(L"ClassName", wc.hInstance);
+    return 0;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# direct9Demo
-一个简单的demo案例
+# 00_案例1/Project1 
+初步案例完成
+
+# 01_创建窗口应用
+窗口应用完成
+
+#  02_第二章 Direct3D程序设计基础\01_CreateDevice
+创建Direct3D设备和初步渲染框架完成。
